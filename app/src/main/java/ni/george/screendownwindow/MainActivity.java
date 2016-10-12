4e22c3e4b6162303e090786e5b1ce23b76f1f386
大城小爱 <237706096@qq.com>
2016-10-12 16:38:21 +0800

    init

---
+++ app/src/main/java/ni/george/screendownwindow/MainActivity.java
@@ -0,0 +1,13 @@
+package ni.george.screendownwindow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
