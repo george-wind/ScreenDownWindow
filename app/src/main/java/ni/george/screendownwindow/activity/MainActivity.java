04a1f8ccbee81b79fad4cf14f50ac9313da56624
大城小爱 <237706096@qq.com>
2016-10-12 20:23:52 +0800

    work

--- app/src/main/java/ni/george/screendownwindow/activity/MainActivity.java
+++ app/src/main/java/ni/george/screendownwindow/activity/MainActivity.java
@@ -1,18 +1,43 @@
 package ni.george.screendownwindow.activity;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import ni.george.screendownwindow.R;
 
 public class MainActivity extends Activity {
+    private Context mContext;
+    private Handler handler=new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext=this;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(mContext,ScreenDownActivity.class);
+                startActivity(intent);
+            }
+        },10*1000);
+
 
     }
+
+
 }
--- app/src/main/java/ni/george/screendownwindow/activity/ScreenDownActivity.java
+++ app/src/main/java/ni/george/screendownwindow/activity/ScreenDownActivity.java
@@ -1,16 +1,57 @@
 package ni.george.screendownwindow.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.KeyguardManager;
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.PowerManager;
+import android.view.Window;
+import android.view.WindowManager;
+
+import ni.george.screendownwindow.R;
 
 /**
  * Created by quan on 2016/10/12.
  */
 public class ScreenDownActivity extends Activity{
+    private Dialog dialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        
+        setContentView(R.layout.activity_screen_down);
+        final Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        createDialog();
+
+
+    }
+
+    void createDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("dfs");
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setMessage("dfsfsfs");
+        dialog=builder.show();
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        if (!pm.isScreenOn()) {
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP |
+                    PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "bright");
+            wl.acquire();
+            wl.release();
+        }
     }
 }
